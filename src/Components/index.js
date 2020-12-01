4d2cd1baac30062ef03463d9649e15b8600f97c8
william jacobs <williamjacobs@williams-MacBook-Pro.local>
2020-11-30 21:34:54 -0800

    first commit for react portfolio

---
+++ src/Components/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import Particles from 'react-particles-js';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    particlesCanvas: {
+        position: 'absolute'
+    }
+})
+
+const Home = () => {
+
+    const classes = useStyles()
+
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <Particles
+            canvasClassName={classes.particlesCanvas}
+                params={{
+                    particles: {
+                        number: {
+                            number: 45,
+                            density: {
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape: {
+                            type: 'circle',
+                            stroke: {
+                                width: 1,
+                                color: 'tomato'
+                            }
+                        },
+                        size: {
+                            value: 8,
+                            random: true,
+                            anim: {
+                                enable: true,
+                                speed: 8,
+                                size_min: 0.1,
+                                sync: true
+                            }
+                        },
+                        
+                    }
+                }}
+            />
+        </>
+    );
+};
+
+export default Home;
